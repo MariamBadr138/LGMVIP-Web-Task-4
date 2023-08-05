d33e4ff0f848ef86df222f71aa74d48c3a06cae8
mariambadr <mariambadr164@gmail.com>
2023-08-05 22:34:05 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+﻿// JavaScript source code
+let show = document.getElementById('show');
+let buttons = Array.from(document.getElementsByClassName('button'));
+
+buttons.map(button => {
+    button.addEventListener('click', (e) => {
+        switch (e.target.innerText) {
+            case 'C':
+                show.innerText = '';
+                break;
+
+            case '←':
+                if (show.innerText) {
+                    show.innerText = show.innerText.slice(0, -1);
+                }
+                break;
+
+            case '=':
+                try {
+                    show.innerText = eval(show.innerText);
+                }
+                catch {
+                    show.innerText = 'Error';
+
+                }
+                break;
+
+
+
+            default:
+                show.innerText += e.target.innerText;
+        }
+    });
+});
